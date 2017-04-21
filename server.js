9fea388f5268ef5d472d3b57dce6b60220954c44
Timo Uhlmann <timouhlmann@gmail.com>
2017-04-21 23:50:12 +0200

    Prepare for some style!

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 // serve static assets from 'public' directory
 app.use(express.static('public'))
+app.use('/styles', express.static('node_modules/normalize.css'))
 
 // start HTTP server on port 3000
 app.listen(3000, () => {
