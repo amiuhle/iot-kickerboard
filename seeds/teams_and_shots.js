26e70acb199dcd67596ce13873d296e676bd2d33
Timo Uhlmann <timouhlmann@gmail.com>
2017-05-17 21:08:04 +0200

    Use team ids 0..4

--- seeds/teams_and_shots.js
+++ seeds/teams_and_shots.js
@@ -1,11 +1,11 @@
 
-// http://www.dfb.de/die-mannschaft/statistik/rekordtorschuetzen/?no_cache=1
+// http://www.dfb.de/die-mannschaft/statistik/rekordtorschuetzen/?no_cache=0
 var teams = [
+  { id: 0, name: 'Team 0' },
   { id: 1, name: 'Team 1' },
   { id: 2, name: 'Team 2' },
   { id: 3, name: 'Team 3' },
-  { id: 4, name: 'Team 4' },
-  { id: 5, name: 'Team 5' }
+  { id: 4, name: 'Team 4' }
 ]
 
 exports.seed = function (knex) {
@@ -19,16 +19,16 @@ exports.seed = function (knex) {
     knex('shots').del()
     .then(function () {
       return knex('shots').insert([
-        { shooter_id: 1, target_id: 5, actual_hit_id: 4 },
-        { shooter_id: 4, target_id: 5, actual_hit_id: 5 },
-        { shooter_id: 5, target_id: 3, actual_hit_id: 3 },
-        { shooter_id: 3, target_id: 5, actual_hit_id: 2 },
+        { shooter_id: 0, target_id: 4, actual_hit_id: 3 },
+        { shooter_id: 3, target_id: 4, actual_hit_id: 4 },
+        { shooter_id: 4, target_id: 2, actual_hit_id: 2 },
         { shooter_id: 2, target_id: 4, actual_hit_id: 1 },
-        { shooter_id: 1, target_id: 2, actual_hit_id: 2 },
-        { shooter_id: 2, target_id: 1, actual_hit_id: 1 },
-        { shooter_id: 1, target_id: 2, actual_hit_id: 3 },
-        { shooter_id: 3, target_id: 5, actual_hit_id: 5 },
-        { shooter_id: 5, target_id: 1 }
+        { shooter_id: 1, target_id: 3, actual_hit_id: 0 },
+        { shooter_id: 0, target_id: 1, actual_hit_id: 1 },
+        { shooter_id: 1, target_id: 0, actual_hit_id: 0 },
+        { shooter_id: 0, target_id: 1, actual_hit_id: 2 },
+        { shooter_id: 2, target_id: 4, actual_hit_id: 4 },
+        { shooter_id: 4, target_id: 0 }
       ])
     })
   ])
